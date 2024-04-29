0653cb44c49776c40b0586b62fa0f709c68a096a
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-04-29 15:23:36 +0200

    [worklife] : add basic header

---
+++ components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="container">
+      <div>logo</div>
+      <div>favorites</div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px red solid;
+  min-height: 3.75rem;
+  padding: 0 3.75rem;
+}
+</style>
--- layouts/default-layout.vue
+++ layouts/default-layout.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "../components/header/header.vue";
+</script>
 
 <template>
-  <div class="layout"></div>
+  <div class="layout">
+    <Header />
+    <div class="layout-content">
+      <slot name="content" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
