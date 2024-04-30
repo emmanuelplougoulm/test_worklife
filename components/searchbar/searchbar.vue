e0740ca00bcc147e26a78ac40808887fc8671c95
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-04-30 16:01:28 +0200

    [worklife] : add basic searchbar component

---
+++ components/searchbar/searchbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="searchbar-container">
+    <input></input>
+    <div>button</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.searchbar-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px blue solid;
+  min-height: 3.75rem;
+}
+
+input {
+  flex:1;
+}
+
+.searchbar-container div {
+  margin-left: 30px;
+  border:1px red solid;
+}
+
+</style>
