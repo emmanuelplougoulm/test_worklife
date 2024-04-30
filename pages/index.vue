a9d51cabe2191dced1f2b0ebbc3b99c0573ba7e8
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-04-30 16:00:55 +0200

    [worklife] : add listing page

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content-container">
+    <SearchBar />
+    <Grid />
+    <div class="button-container"><Button /></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SearchBar from "../components/searchbar/searchbar.vue";
+import Grid from "../components/grid/grid.vue";
+import Button from "../components/button/button.vue";
+</script>
+
+<style>
+.content-container {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 30px 60px;
+  background-color: aqua;
+  border: 2px solid red;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
