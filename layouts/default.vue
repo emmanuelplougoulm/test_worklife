b976b1d825ec71f11894cc1ec6b4077725d7bf09
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-04-30 11:48:12 +0200

    [worklife] : set default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Header from "../components/header/header.vue";
+</script>
+
+<template>
+  <div class="layout-container">
+    <Header />
+    <slot />
+  </div>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  border: 2px green solid;
+}
+
+.layout-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  border: 5px black solid;
+}
+</style>
