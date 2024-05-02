bccda1a9052055cc0ab84665acae020844a05365
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-02 10:53:33 +0200

    [worklife] : fix details page display

--- pages/details/[id]/index.vue
+++ pages/details/[id]/index.vue
@@ -1,32 +1,47 @@
 <template>
   <div class="container">
     <div>
-      <button @click="navigateTo(`/`)">I am the back button</button>
+      <Button @click="navigateTo(`/`)" :text="'Back'" />
     </div>
-    <div class="modal-body">
-      <div class="image">image</div>
+    <div class="details-container">
+      <div class="image image-tile">
+        <!-- <img :src="img_url" class="image" /> -->
+      </div>
       <div class="infos">
-        <div class="">title</div>
+        <div class="title">{{ currentArtwork.title }}</div>
         <div class="">description</div>
         <div class="">category</div>
         <div class="tags">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
+          <div class="tag">Tag 1</div>
+          <div class="tag">Tag 2</div>
+          <div class="tag">Tag 3</div>
+          <div class="tag">Tag 4</div>
         </div>
       </div>
     </div>
     <div class="favorites-container">
       <div class="empty"></div>
       <div class="btn-container">
-        <button>I am the favourites button</button>
+        <Button @click="addToFavorites" :text="'AddToFavorites'" />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStore } from "@/stores/store";
+import Button from "@/components/button/button.vue";
+
+const store = useStore();
+
+const currentArtwork: any = store.currentArtwork;
+
+// const img_url = currentArtwork.webImage.url ? currentArtwork.webImage.url : "";
+
+const addToFavorites = () => {
+  console.log("add to favorites");
+};
+</script>
 
 <style>
 .container {
@@ -35,8 +50,8 @@
   flex-direction: column;
   gap: 20px;
   padding: 30px 60px;
-  background-color: greenyellow;
-  border: 2px solid red;
+  background-color: #ffffff;
+  border-radius: 8px;
 }
 
 .button-container {
@@ -44,8 +59,7 @@
   justify-content: center;
 }
 
-.modal-body {
-  border: 1px red solid;
+.details-container {
   display: flex;
   min-height: 300px;
 }
@@ -60,12 +74,12 @@
   flex: 2;
 }
 
-.modal-body .image {
+.details-container .image {
   flex: 1;
   border: 1px black solid;
 }
 
-.modal-body .infos {
+.details-container .infos {
   flex: 2;
   border: 1px black solid;
 }
@@ -73,11 +87,39 @@
 .tags {
   display: flex;
   flex-wrap: wrap;
+  gap: 5px;
 }
 
-.tags div {
-  border: 1px brown solid;
-  flex-basis: 33%;
+.tags .tag {
+  background-color: #ffffff;
+  border-radius: 4px;
+  border: 1px black solid;
+  flex-basis: 25%;
+  padding: 4px;
+}
+
+.tag:hover {
+  background-color: pink;
+}
+
+.image-tile {
+  position: relative;
+  overflow: hidden;
+  width: 100px;
+  height: 100px;
+}
+
+.image-tile img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.infos .title {
+  font-size: 20px;
+  font-weight: 600;
+  text-transform: uppercase;
+  flex: 2;
 }
 
 @media screen and (max-width: 768px) {
